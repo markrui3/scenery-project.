61ac37932987c19974612e4ea23c9c5e3ca02cf9
toddlt <tju.liutong@gmail.com>
2015-05-27 14:35:27 +0800

    查询景点的功能基本完成，可以跳转了

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -11,13 +11,31 @@ class IndexController extends Controller {
     }
 
     public function loclist(){
-    	$this->display('Index/list');
+        $scenery_id = I('get.scenery_id');
+        $Sub_scenery = M('sub_scenery');
+        $sub_scenery_data = $Sub_scenery->where("scenery_id=\"$scenery_id\"")->select();
+        $this->assign('sub_scenery_data',$sub_scenery_data);
+    	$this->display('list');
     }
 
-    public function searchScenery(){
+    public function searchSceneryByPlace(){
     	$province = I('get.province');
     	$city = I('get.city');
     	$Scenery = M('scenery');
-    	echo json_encode($Scenery->where("province=\"$province\" AND city=\"$city\"")->select());
+        if ($city == ""){
+            echo json_encode($Scenery->where("province=\"$province\"")->select());
+        } else {
+    	    echo json_encode($Scenery->where("province=\"$province\" AND city=\"$city\"")->select());
+        };
+    }
+
+    public function searchSceneryByName(){
+        $scenery_name = I('get.scenery_name');
+        $Scenery = M('scenery');
+        if($scenery_name != ''){
+            $sql = "select * from scenery where scenery_name like '%$scenery_name%'";
+            echo json_encode($Scenery->query($sql));
+        }
+        
     }
 }
