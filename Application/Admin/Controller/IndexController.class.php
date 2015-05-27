cdaa9fb8808a02a6d610a5be15dce5ff458ccfb6
markrui <87541708@qq.com>
2015-05-27 20:16:15 +0800

    前端统一化，没有add，都是edit

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -3,15 +3,17 @@ namespace Admin\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
+        $Dao = M('scenery');
+        $result = $Dao->select();
+        foreach ($result as &$r) {
+            $r['sub_query'] = '<a href="{:U(\'/Admin/Index/sublist/sub_scenery_id/'.$r['scenery_id'].'\')}">查看</a>';
+            $r['sub_add'] = '<a href="{:U(\'/Admin/Index/editSub/\')}">添加</a>';
+        }
         $this->display('index');
     }
 
     public function login(){
         $this->display('login');
-    }
-
-    public function add(){
-        $this->display('add');
     }
 
     public function edit(){
@@ -20,5 +22,9 @@ class IndexController extends Controller {
 
     public function sublist(){
         $this->display('sublist');
+    }
+
+    public function editSub(){
+        $this->display('editSub');
     }
 }
--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -3,7 +3,7 @@ return array(
 	//'配置项'=>'配置值'
 	'DB_TYPE'   => 'mysql', // 数据库类型
     'DB_HOST'   => '121.193.130.122', // 服务器地址
-    'DB_NAME'   => 'scenery-project', // 数据库名
+    'DB_NAME'   => 'scenery_project', // 数据库名
     'DB_USER'   => 'root', // 用户名
     'DB_PWD'    => 'root', // 密码
     'DB_PORT'   => 3306, // 端口
