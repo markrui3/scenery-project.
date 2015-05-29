eff71b9a4b64f8cee7b037740d70c3e881f07155
markrui <87541708@qq.com>
2015-05-29 20:04:18 +0800

    大致跳转，datatable谁特么写的！

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -11,8 +11,8 @@ class IndexController extends Controller {
         $Dao = M('scenery');
         $result = $Dao->select();
         foreach ($result as &$r) {
-            $r['sub'] = '<a href='.U('/Admin/Index/sublist/scenery_id/'.$r['scenery_id']).'>查看</a><a href='.U('/Admin/Index/editSub/').'>添加</a>';
-            $r['operation'] = '<a href="{:U(\'/Admin/Index/edit/\')}">编辑</a><a href="#" onclick="del(\'$r[\'scenery_id\']\')">删除</a>';
+            $r['sub'] = '<a href='.U('/Admin/Index/sublist/scenery_id/'.$r['scenery_id']).'>查看</a><a href='.U('/Admin/Index/subEdit/scenery_id/'.$r['scenery_id']).'>添加</a>';
+            $r['operation'] = '<a href='.U('/Admin/Index/edit/scenery_id/'.$r['scenery_id']).'>编辑</a><a href="#" onclick="del(\'$r[\'scenery_id\']\')">删除</a>';
         }
         echo json_encode($result);
     }
