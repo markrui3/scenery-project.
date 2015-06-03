14d272064ba10dc3a9b1e8da54e58849775a2ded
Catold <catoldcui@gmail.com>
2015-06-03 18:20:06 +0800

    移动端辨别

--- Application/Home/Controller/WechatController.class.php
+++ Application/Home/Controller/WechatController.class.php
@@ -21,7 +21,6 @@ class WechatController extends Controller {
 	}
 
 	public function index(){
-		
 		$this->weObj->valid();//明文或兼容模式可以在接口验证通过后注释此句，但加密模式一定不能注释，否则会验证失败
 
 		// // Log::Write('type:'.$type, 'INFO');
